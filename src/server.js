c72331e1c0dc73c34c606c00666a39c40046d7af
AKAASH M K <mkaksh0@gmail.com>
2025-11-05 06:11:36 +0530

    Fix Google OAuth redirect - always return code

--- src/server.js
+++ src/server.js
@@ -209,5 +209,5 @@ io.on("connection", (socket) => {
 // ========================
 // 🚀 Start Server
 // ========================
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ;
 httpServer.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
